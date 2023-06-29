86f30087a42eab71f19437ea7e07d421b56f36bd
brennoliveira <brenno.oliveira@souunit.com.br>
2023-06-29 19:00:54 -0300

    initial commit

---
+++ tiny-link-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return <Home />
+}
+export default App;
