91b4fcecb59257945b07dffcc3614b30629a5f9f
brennoliveira <brenno.oliveira@souunit.com.br>
2023-07-04 13:21:31 -0300

    add deploy config

---
+++ deploy.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+fs.copyFileSync(path.join('src', 'index.jsx'), path.join('src', 'index.html'));
+
+execSync('npm run build', { stdio: 'inherit' });
+
+execSync('mv build docs', { stdio: 'inherit' });
+
+fs.copyFileSync(path.join('src', 'index.html'), path.join('docs', '404.html'));
+
+console.log('Deployment complete!');
