4ba0084a181670306b43a7a32976711ff907a14f
brennoliveira <brenno.oliveira@souunit.com.br>
2023-07-07 18:31:31 -0300

    delete NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import RedirectRoute from './pages/RedirectRoute';
-import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
